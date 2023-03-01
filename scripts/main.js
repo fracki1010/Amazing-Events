189585c11fe10a6e17bb897cb511b02ae56daa52
Franco Galdame <fracki1010@gmail.com>
2023-03-01 19:36:11 -0300

    add java Script

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+let num1 = 0;
+let num2= 0;
+
+num1 = prompt("ingrese un numero: ");
+num2 = prompt("ingrese otro numero: ");
+
+/* if(num1 > num2){
+    console.log("el primer numero es mayor");
+}else if(num1 < num2){
+    console.log("el segundo numero es mayor");
+}else{
+    console.log("los numero son iguales");
+} */
+
+console.log(num1);
