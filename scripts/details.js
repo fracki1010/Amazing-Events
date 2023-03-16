93082fc8e8f690ec9445b6351a975d8d8a654f52
Franco Galdame <fracki1010@gmail.com>
2023-03-16 15:49:41 -0300

    arreglo de maquetacion

--- scripts/details.js
+++ scripts/details.js
@@ -11,16 +11,16 @@ console.log(tarjetaDetalles);
 let contenedorDetalle = document.getElementById("detalles");
 let string = '';
 string +=
-    `<div class="card p-3 d-flex " style="width: 10000px; height: auto;">
-    <div class="row g-0">
-     <div class="col-sm-6 col-12">
+    `<div class="card p-3 d-flex imgDetalles" style="width: 10000px; height: auto;">
+        <div class="row g-0">
+        <div class="col-sm-6 col-12">
         <img src="${tarjetaDetalles.image}" id="" style="width: 100%; height: auto;"
-      class="img-fluid rounded-start" alt="...">
+        class="img-fluid rounded-start" alt="...">
   </div>
   <div class="col-sm-6 col-12">
     <div class="card-body text-center fs-5">
       <h5 class="card-title fw-bold fs-1 tituloDetalles ">${tarjetaDetalles.name}</h5>
-      
+      <p class="card-text text-center fs-5 ps-2">${tarjetaDetalles.description}</p>
       <p class="card-text text-start fs-5 ps-2">Capacity: ${tarjetaDetalles.capacity}</p>
       <p class="card-text text-start fs-5 ps-2">Place Room: ${tarjetaDetalles.place}</p>
       <p class="card-text text-start fs-5 ps-2">Asistance: ${tarjetaDetalles.assistance}</p>
